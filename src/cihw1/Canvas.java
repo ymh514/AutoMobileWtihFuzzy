7d58a561f6dc8baa4726bef16fcc37f1872b963b
Terry You <microvoyager@hotmail.com>
2016-03-27 17:25:46 +0800

    Update Canvas.java

--- src/cihw1/Canvas.java
+++ src/cihw1/Canvas.java
@@ -99,7 +99,85 @@ public class Canvas extends Pane{
 		line8.setStroke(Color.GRAY);
 		line8.setStrokeWidth(3);
 		this.getChildren().add(line8);
+	}
+	public void rePaint(){
+		
+		this.getChildren().clear();
+		
+		line1 = new Line();
+		line1.setStartX(24 * ratio);
+		line1.setStartY(52 * ratio);
+		line1.setEndX(36 * ratio);
+		line1.setEndY(52 * ratio);
+		line1.setStroke(Color.BLACK);
+		line1.setStrokeWidth(3);
+		this.getChildren().add(line1);
 
+		line2 = new Line();
+		line2.setStartX(24 * ratio);
+		line2.setStartY(52 * ratio);
+		line2.setEndX(24 * ratio);
+		line2.setEndY(30 * ratio);
+		line2.setStroke(Color.GRAY);
+		line2.setStrokeWidth(3);
+		this.getChildren().add(line2);
 
+		line3 = new Line();
+		line3.setStartX(36 * ratio);
+		line3.setStartY(52 * ratio);
+		line3.setEndX(36 * ratio);
+		line3.setEndY(42 * ratio);
+		line3.setStroke(Color.GRAY);
+		line3.setStrokeWidth(3);
+
+		this.getChildren().add(line3);
+
+		line4 = new Line();
+		line4.setStartX(24 * ratio);
+		line4.setStartY(30 * ratio);
+		line4.setEndX(48 * ratio);
+		line4.setEndY(30 * ratio);
+		line4.setStroke(Color.GRAY);
+		line4.setStrokeWidth(3);
+
+		this.getChildren().add(line4);
+
+		line5 = new Line();
+		line5.setStartX(36 * ratio);
+		line5.setStartY(42 * ratio);
+		line5.setEndX(60 * ratio);
+		line5.setEndY(42 * ratio);
+		line5.setStroke(Color.GRAY);
+		line5.setStrokeWidth(3);
+
+		this.getChildren().add(line5);
+
+		line6 = new Line();
+		line6.setStartX(48 * ratio);
+		line6.setStartY(30 * ratio);
+		line6.setEndX(48 * ratio);
+		line6.setEndY(15 * ratio);
+		line6.setStroke(Color.GRAY);
+		line6.setStrokeWidth(3);
+
+		this.getChildren().add(line6);
+		
+		line7 = new Line();
+		line7.setStartX(60 * ratio);
+		line7.setStartY(42 * ratio);
+		line7.setEndX(60 * ratio);
+		line7.setEndY(15 * ratio);
+		line7.setStroke(Color.GRAY);
+		line7.setStrokeWidth(3);
+		this.getChildren().add(line7);
+		
+		line8 = new Line();
+		line8.setStartX(48 * ratio);
+		line8.setStartY(15 * ratio);
+		line8.setEndX(60 * ratio);
+		line8.setEndY(15 * ratio);
+		line8.setStroke(Color.GRAY);
+		line8.setStrokeWidth(3);
+		this.getChildren().add(line8);
 	}
 }
